848901904a381fbc58a4ea096dbe5803878e41f0
rohansharma99 <rohansharmaji9411@gmail.com>
2025-11-16 10:06:11 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  //  base: '/myportfolio',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
