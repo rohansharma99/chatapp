6ae6278eb9ed49b9d97b8e46ba2d6e50620f8bf4
rohansharma99 <rohansharmaji9411@gmail.com>
2025-11-16 10:15:47 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-
+import React from 'react'
 import Main from './component/main/main'
 import Sidebar from './component/Sidebar/Sidebar'
 import './index.css'
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  //  base: '/myportfolio',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  plugins: [
+    tailwindcss(),
+  ],
+})
